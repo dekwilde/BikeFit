083cc3044a1b07dfe6131a03d6987005c9794b10
jake.walsh <jlwalsh88@gmail.com>
2013-09-22 14:17:04 -0700

    Added depth decoder

--- src/ZigStreamDecoders.js
+++ src/ZigStreamDecoders.js
@@ -2,25 +2,53 @@
  * Decoders for zig.js sensor's streams (rgb, depth)
  */
 
+// http://stackoverflow.com/questions/9543248/access-kinect-rgb-image-data-from-zigjs
+var bufferWidth = 160;
+var bufferHeight = 120;
+
 /**
- * Decoders the sensor's image stream (rgb)
+ * Decodes the sensor's image stream (rgb)
  * @param zigPlugin
  * @returns {HTMLElement} canvas containing the decoded image
  */
 function imageToCanvas(zigPlugin) {
+    return toCanvas(zigPlugin.imageMap, function(data, srcData, length) {
+        for (var i = 0; i < length; i++) {
+            data[i * 4] = srcData[i * 3 + 2];
+            data[i * 4 + 1] = srcData[i * 3 + 1];
+            data[i * 4 + 2] = srcData[i * 3 + 0];
+            data[i * 4 + 3] = 255; // no alpha
+        }
+    });
+}
+
+/**
+ * Decode the sensors's depth stream
+ * @param zigPlugin
+ * @returns {HTMLElement} canvas containing the decoded image
+ */
+function depthToCanvas(zigPlugin) {
+    return toCanvas(zigPlugin.depthMap, function(data, srcData, length) {
+        for (var i = 0; i < length; i++) {
+            data[i * 4] = srcData[i * 2 + 0];
+            data[i * 4 + 1] = srcData[i * 2 + 1];
+            data[i * 4 + 2] = 255;
+            data[i * 4 + 3] = 255;
+        }
+    });
+}
+
+
+function toCanvas(stream, decodeFunction) {
     var buffer = document.createElement('canvas');
+    buffer.width = bufferWidth;
+    buffer.height = bufferHeight;
+
     var ctx = buffer.getContext("2d");
-    buffer.width = 160;
-    buffer.height = 120;
-    var pix = ctx.createImageData(160, 120); // http://stackoverflow.com/questions/9543248/access-kinect-rgb-image-data-from-zigjs
+    var pix = ctx.createImageData(buffer.width, buffer.height);
     var data = pix.data;
-    var srcData = Base64.decode(zigPlugin.imageMap);
-    for (var i = 0; i < 160 * 120; i++) {
-        data[i * 4] = srcData[i * 3 + 2];
-        data[i * 4 + 1] = srcData[i * 3 + 1];
-        data[i * 4 + 2] = srcData[i * 3 + 0];
-        data[i * 4 + 3] = 255;
-    }
+    var srcData = Base64.decode(stream);
+    decodeFunction(data, srcData, buffer.width * buffer.height)
     ctx.putImageData(pix, 0, 0);
     return buffer;
 }
